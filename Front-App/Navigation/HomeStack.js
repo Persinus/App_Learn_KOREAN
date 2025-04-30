68f9af73d41846ac132d74b6a90fc7a0c7ad2541
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-01 01:08:32 +0700

    Update dark mode in HomeStack.js

--- Front-App/Navigation/HomeStack.js
+++ Front-App/Navigation/HomeStack.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useSelector } from 'react-redux';
 import HomeScreen from '../Screens/Home/HomeScreen';
 import NotificationsScreen from '../Screens/Notifications/NotificationsScreen';
 import DailyReward from '../Screens/Home/DailyReward';
@@ -7,11 +8,35 @@ import DailyReward from '../Screens/Home/DailyReward';
 const Stack = createStackNavigator();
 
 const HomeStack = () => {
+  const isDarkMode = useSelector((state) => state.darkMode.isDarkMode);
+
+  const screenOptions = {
+    headerStyle: {
+      backgroundColor: isDarkMode ? '#6666FF' : '#4b46f1', // Màu tím cho header
+    },
+    headerTintColor: isDarkMode ? '#fff' : '#fff', // Màu chữ header
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      <Stack.Screen name="NotificationsScreen" component={NotificationsScreen} />
-      <Stack.Screen name="DailyReward" component={DailyReward} />
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen 
+        name="HomeScreen" 
+        component={HomeScreen} 
+        options={{ title: 'Trang chủ' }} 
+      />
+      <Stack.Screen 
+        name="NotificationsScreen" 
+        component={NotificationsScreen} 
+        options={{ title: 'Thông báo' }} 
+      />
+      <Stack.Screen 
+        name="DailyReward" 
+        component={DailyReward} 
+        options={{ title: 'Phần thưởng hàng ngày' }} 
+      />
     </Stack.Navigator>
   );
 };
