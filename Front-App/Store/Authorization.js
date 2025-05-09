3eb531171e87415459e422a13bdaf96db830d228
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-09 15:06:21 +0700

    Update Authorization.js

--- Front-App/Store/Authorization.js
+++ Front-App/Store/Authorization.js
@@ -1,41 +1,25 @@
+// Front-App/Store/Authorization.js
 import { createSlice } from '@reduxjs/toolkit';
 
-// Trạng thái ban đầu
 const initialState = {
   isLoggedIn: false,
   token: null,
 };
 
-// Tạo slice cho Authorization
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
     login: (state, action) => {
       state.isLoggedIn = true;
-      state.token = action.payload; // Lưu token
-      localStorage.setItem('token', action.payload); // Lưu token vào localStorage
+      state.token = action.payload;
     },
     logout: (state) => {
       state.isLoggedIn = false;
       state.token = null;
-      localStorage.removeItem('token'); // Xóa token khỏi localStorage
-    },
-    checkAuth: (state) => {
-      const token = localStorage.getItem('token');
-      if (token) {
-        state.isLoggedIn = true;
-        state.token = token;
-      } else {
-        state.isLoggedIn = false;
-        state.token = null;
-      }
     },
   },
 });
 
-// Export các action
-export const { login, logout, checkAuth } = authSlice.actions;
-
-// Export reducer
+export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
