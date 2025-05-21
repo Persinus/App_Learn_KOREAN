957023637d217634bb680ab9914af8258d4594d7
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-22 04:34:31 +0700

    Chỉnh sửa: Store.js - cập nhật quản lý trạng thái

--- Store/Store.js
+++ Store/Store.js
@@ -2,13 +2,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import darkModeReducer from './DarkMode';
 import languageReducer from './Language';
-import authReducer from './Authorization';
+
 
 const store = configureStore({
   reducer: {
     darkMode: darkModeReducer,
     language: languageReducer,
-    auth: authReducer,
+  
   },
 });
 
