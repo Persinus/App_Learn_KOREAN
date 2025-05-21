59bfd63056093b853e064f4f2b30835881533b66
Persinus <nguyenmanh2004devgame@gmail.com>
2025-05-22 04:34:18 +0700

    Chỉnh sửa: RankingsStack.js - cập nhật điều hướng xếp hạng

--- Navigation/RankingsStack.js
+++ Navigation/RankingsStack.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import RankingsScreen from '../Screens/Rankings/RankingsScreen';
-import DetailRanking from '../Screens/Rankings/DetailRanking';
+
 
 const Stack = createStackNavigator();
 
@@ -10,7 +10,7 @@ const RankingsStack = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="RankingsScreen" component={RankingsScreen} />
-      <Stack.Screen name="DetailRanking" component={DetailRanking} />
+
     </Stack.Navigator>
   );
 };
